0cf42a523c96a1f8dc21f62f08674ec0a8121d74
yaSecret <418646120@qq.com>
2018-01-03 23:32:34 +0800

    选择排序

--- src/com/madjava/spider/demo0/Spider0.java
+++ src/com/madjava/spider/demo0/Spider0.java
@@ -50,24 +50,16 @@ public class Spider0 {
         return result;
     }
     //用正则表达式处理字符串获取想要的内容
-    public static List<Zhihu> getZhihu(String targetStr){
+    public static List<Zhihu> GetRecommendations(String targetStr){
         List<Zhihu> results = new ArrayList<>();
-        //匹配标题
-        Pattern questionPattern = Pattern.compile("question_link.+?>\\s(.+?)\\s<");
-        //定义一个matcher来做匹配
-        Matcher questionMatcher = questionPattern.matcher(targetStr);
         //匹配url，也就是链接的问题
-        Pattern urlPattern = Pattern.compile("question_link.+?href=\\\"(.+?)\\\"");
-        Matcher urlMatcher = urlPattern.matcher(targetStr);
-        //问题和链接要同时匹配到
-        boolean isFind = questionMatcher.find()&&urlMatcher.find();
-        Zhihu tempZhihu = null;
-        while (isFind){
-            tempZhihu = new Zhihu();
-            tempZhihu.setQuestion(questionMatcher.group(1));
-            tempZhihu.setZhihuUrl("\"http://www.zhihu.com\""+urlMatcher.group(1));
-            results.add(tempZhihu);
-            isFind = questionMatcher.find() && urlMatcher.find();
+        Pattern pattern = Pattern.compile("<h2>.+?post-link.+?href=\"(.+?)\".+?</h2>");
+        Matcher matcher = pattern.matcher(targetStr);
+        while (matcher.find()){
+            //定义一个知乎对象来存储抓取到的信息
+            Zhihu ZhihuTemp = new Zhihu(matcher.group(1));
+            //添加成功匹配的结果
+            results.add(ZhihuTemp);
         }
         return results;
     }
@@ -76,7 +68,7 @@ public class Spider0 {
         String content = sentGet(url);
 //        System.out.println(content);
         /*String imgSrc = RegaxString(result,"question_link.+?>\\s(.+?)\\s<");*/
-        List<Zhihu> myZhihu = getZhihu(content);
+        List<Zhihu> myZhihu = GetRecommendations(content);
         System.out.println(myZhihu);
     }
 }
--- src/com/madjava/spider/demo0/Zhihu.java
+++ src/com/madjava/spider/demo0/Zhihu.java
@@ -1,6 +1,8 @@
 package com.madjava.spider.demo0;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Administrator on 2017/12/31.
@@ -8,6 +10,7 @@ import java.util.ArrayList;
 public class Zhihu {
     private String question;//问题
     private String zhihuUrl;//链接
+    private String questionDescription;// 问题描述
     private ArrayList<String> answers;//存储所有回答问题的数组
 
     public String getQuestion() {
@@ -26,16 +29,53 @@ public class Zhihu {
         this.zhihuUrl = zhihuUrl;
     }
 
-    public Zhihu() {
+    public Zhihu(String url) {
+        question = "";
         question = "";
         zhihuUrl = "";
         answers = new ArrayList<>();
+        if(getRealUrl(url)){
+            System.out.println("正则抓取"+zhihuUrl);
+            String content = Spider0.sentGet(zhihuUrl);
+            Pattern pattern;
+            Matcher matcher;
+            //匹配标题
+            pattern = Pattern.compile("zh-question-title.+?<h2.+?>(.+?)</h2>");
+            matcher = pattern.matcher(content);
+            if(matcher.find()){
+                question =  matcher.group(1);
+            }
+            //匹配描述
+            pattern = Pattern.compile("zh-question-detail.+?<div.+?>(.*?)</div>");
+            matcher = pattern.matcher(content);
+            if(matcher.find()){
+                question =  matcher.group(1);
+            }
+            //匹配答案
+            pattern = Pattern.compile("/answer/content.+?<div.+?>(.*?)</div>");
+            matcher = pattern.matcher(content);
+            if(matcher.find()){
+                answers.add(matcher.group(1));
+            }
+        }
     }
-
+    //处理url
+    public boolean getRealUrl(String url){
+        Pattern pattern = Pattern.compile("question/(.*?)/");
+        Matcher matcher = pattern.matcher(url);
+        if(matcher.find()){
+            zhihuUrl = "http://www.zhihu.com/question/"+matcher.group(1);
+        }else {
+            return false;
+        }
+        return true;
+    }
     public String  toString(){
         StringBuffer sb = new StringBuffer();
-        sb.append("问题").append(question).append("\n链接").append(zhihuUrl)
-                 .append("\n回答").append(answers).append("\n");
+        sb.append("问题").append(question)
+                .append("\n描述").append(questionDescription)
+                .append("\n链接").append(zhihuUrl)
+                .append("\n回答").append(answers).append("\n");
         return sb.toString();
     }
 }
